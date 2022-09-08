ceba018c5a40574cb4efcf0aef6b0a4165dfa233
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-09-08 15:32:42 +0100

    added factorial via recursion

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using DotNetAlgo.Sorts;
-
+using DotNetAlgo.Recursion;
 class Program
 {
 
 
     public static void Main()
     {
-        var bubbleSort = InsertionSort.WithForLoop(new int[] { 0, 6, 9, 8, 7, 2, 1, 5, 3, 4});
-        Console.WriteLine(bubbleSort);
+        // var bubbleSort = InsertionSort.WithForLoop(new int[] { 0, 6, 9, 8, 7, 2, 1, 5, 3, 4});
+        var factorial = Factorial.Solve(5);
+        Console.WriteLine(factorial);
     }
 }
---
+++ Recursion/Factorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAlgo.Recursion
+{
+    internal class Factorial
+    {
+        public static int Solve(int number)
+        {
+            if (number == 2) return number;
+            return number * Solve(number - 1);
+        }
+    }
+}
